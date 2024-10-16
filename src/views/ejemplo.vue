ee9820339f79210f537a0faeb454bd3c61217dc1
camilo reyes <camiloreyesmorales01@gmail.com>
2024-10-15 20:37:13 -0500

    Class 18/09/24

---
+++ src/views/ejemplo.vue
@@ -0,0 +1,16 @@
+<script>
+// Logica JS
+
+export default{
+    name:'ejemplo'
+}
+</script>
+
+<template>
+    <!--visualizar los formularios HTML-->
+    <h1>modulo de cargos</h1>
+</template>
+
+<style>
+/* estilo css */
+</style>
