f926f0455a6a924d7e9fc94bb238af857235b80a
camilo reyes <camiloreyesmorales01@gmail.com>
2024-10-18 19:34:51 -0500

    add components

---
+++ src/components/Empleado.vue
@@ -0,0 +1,12 @@
+<script>
+// Logica JS
+</script>
+
+<template>
+    <!--visualizar los formularios HTML-->
+    <h1>modulo de empleados</h1>
+</template>
+
+<style>
+/* estilo css */
+</style>
---
+++ src/components/Producto.vue
@@ -0,0 +1,12 @@
+<script>
+// Logica JS
+</script>
+
+<template>
+    <!--visualizar los formularios HTML-->
+    <h1>modulo de cargos</h1>
+</template>
+
+<style>
+/* estilo css */
+</style>
